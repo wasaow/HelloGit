902ca775fbbd5b41a5a31114bb52d43e7e5f9791
Calvin <Calvin@10.69.78.3>
2021-08-30 16:11:50 -0500

    Added Calvin's name and major to the output

--- src/Hello.java
+++ src/Hello.java
@@ -12,5 +12,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Winston");
 		System.out.println("Computer Science");
-	}
+		System.out.println("Calvin");
+		System.out.println("Computer Science");
+			}
 }
